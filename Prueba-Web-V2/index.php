8779d2f1d1b46c63a7efa7461802e2a4b4d7c4fb
Igor Llancapan <55776451+igONELL@users.noreply.github.com>
2020-05-05 03:09:00 -0400

    Prueba-Web-V2
    Prueba -version 2.0 añadiendo Nicolas Ardila
    
    Co-Authored-By: nardilab11 <nardilab11@users.noreply.github.com>

---
+++ Prueba-Web-V2/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+<head> 
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="author" content="Nicolas Ardila - Igor Llancapan - Francisco Fernadez" />
+	<link rel="stylesheet" href="estilo/estilo.css">
+	<title>Blog de video Juegos</title>
+</head>
+<body>
+	<div class="contenedor">
+		
+		<header>
+			<a href="index.php"><img src="imagenes/logo.png" alt=""></a>
+			<h1>Blogs de Video juegos</h1>
+		</header>
+		<article>	
+
+		<a href="pruebaNico/index.php"><img src="imagenes/yakuza-0.png"></a>
+
+		<a href="pruebaIgor/index.php"><img src="imagenes/csgo.png"></a>
+
+		<img src="imagenes/pubg.png" alt="">
+
+		</article>
+
+		<footer>
+			<h3>Desarrollado por Nicolas-Igor-Francisco. Programación Web.</h3>
+		</footer>
+	</div>
+	
+</body>
+</html>
---
+++ Prueba-Web-V2/pruebaIgor/header.php
@@ -0,0 +1,12 @@
+
+	<header>
+	<div id="csgo">	
+		<img src="imagenes/csgo.png" alt="">
+	</div>
+			<nav>
+				<ul>
+					<li><a href="index.php">Blog</a></li>
+					<li><a href="../index.php">Inicio</a></li>
+				</ul>
+			</nav>		
+	</header>
