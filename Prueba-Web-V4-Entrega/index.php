a876ff18f0a9da59aa29c773b0eb439cc2a9a783
Igor Llancapan <55776451+igONELL@users.noreply.github.com>
2020-05-05 11:50:43 -0400

    Entrega-final-web
    Entrega final para programcion web. Nicolas Ardila, Francisco Fernandez, Igor Llancapan
    
    Co-Authored-By: nardilab11 <nardilab11@users.noreply.github.com>
    Co-Authored-By: ukka199 <ukka199@users.noreply.github.com>

---
+++ Prueba-Web-V4-Entrega/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+<head> 
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="author" content="Nicolas Ardila - Igor Llancapan - Francisco Fernadez" />
+	<link rel="icon" href="imagenes/favi.icon.png" type="image/x-icon">
+	<link rel="stylesheet" href="estilo/estilo.css">
+	<title>Blog de video Juegos</title>
+</head>
+<body>
+	<div class="contenedor">
+		
+		<header>
+			<a href="index.php"><img src="imagenes/logo.png" alt=""></a>
+			<h1>Blogs de Video juegos</h1>
+		</header>
+		<article>	
+
+		<a href="pruebaNico/nico.php"><img src="imagenes/yakuza-0.png"></a>
+
+		<a href="pruebaIgor/igor.php"><img src="imagenes/csgo.png"></a>
+
+		<a href="pruebaFrancisco/francisco.php"><img src="imagenes/pubg.png" alt=""></a>
+
+		</article>
+
+		<footer>
+			<h3>Desarrollado por Nicolas Ardila - Igor Llancapan - Francisco Fernandez. Programación Web.</h3>
+		</footer>
+	</div>
+	
+</body>
+</html>
---
+++ Prueba-Web-V4-Entrega/pruebaIgor/header.php
@@ -0,0 +1,12 @@
+
+	<header>
+	<div id="csgo">	
+		<a href="igor.php"><img src="imagenes/csgo.png" alt=""></a>
+	</div>
+			<nav>
+				<ul>
+					<li><a href="igor.php">Blog</a></li>
+					<li><a href="../index.php">Inicio</a></li>
+				</ul>
+			</nav>		
+	</header>
